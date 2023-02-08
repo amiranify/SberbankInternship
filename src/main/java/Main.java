1cb9026abb4f098a657e9d1960431347a72c467c
amiranify <110248739+amiranify@users.noreply.github.com>
2023-02-08 19:09:00 +0300

    сортировка по списку городов и федеральных округов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,38 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String... args) {
         CityUtils cityUtils = new CityUtils();
         List<City> cities = cityUtils.parse();
-        print(cities);
-    }
 
-    public static void print(List<City> cities) {
+        /**
+         * Сортировка списка городов по наименованию
+         * в алфавитном порядке по убыванию без учета регистра;
+         * вариант реализации Comparator
+         */
+        cities.sort(Comparator.comparing(City::getName));
         cities.forEach(System.out::println);
+        System.out.println();
+
+        /**
+         * Сортировка списка городов по федеральному округу и наименованию города внутри каждого федерального
+         * округа в алфавитном порядке по убыванию с учетом регистра;
+         * вариант реализации Comparator
+         */
+
+        cities.sort(Comparator.comparing(City::getDistrict).thenComparing(City::getName));
+        cities.forEach(System.out::println);
+        System.out.println();
+
+        /**
+         * Сортировка списка городов по наименованию
+         * в алфавитном порядке по убыванию без учета регистра;
+         * вариант реализации lambda-выражения
+         */
+
+        cities.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
+        cities.forEach(System.out::println);
+        System.out.println();
     }
 }
